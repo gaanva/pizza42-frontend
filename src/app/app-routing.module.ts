8105d091fe384997f6f7b9e06d365879df10825e
Antornio <gaanva@gmail.com>
2019-05-11 14:41:56 -0300

    Updating routing with authorization.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListPizzasComponent } from './list-pizzas/list-pizzas.component';
 import { CallbackComponent } from './callback/callback.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
+import { ScopeGuardService as ScopeGuard } from './auth/scope-guard.service';
 
 const routes: Routes = [
   { path: '', component: ListPizzasComponent, pathMatch: 'full' },
   { path: 'callback', component: CallbackComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [ScopeGuard], data: { expectedScopes: ['write:messages']} },
   { path: '**', redirectTo: '' }
 ];
 
