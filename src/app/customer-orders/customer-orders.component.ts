11ac980b048cc7d2be22813b08b5e02357fff90d
Antornio <gaanva@gmail.com>
2019-05-14 23:59:27 -0300

    adding orders and my order request

--- src/app/customer-orders/customer-orders.component.ts
+++ src/app/customer-orders/customer-orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../list-pizzas/order';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-customer-orders',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-orders.component.css']
 })
 export class CustomerOrdersComponent implements OnInit {
+  orders:Order[];
+  API_URL: string = "http://localhost:3001/";
 
-  constructor() { }
-
+  constructor(public auth:AuthService, private http: HttpClient) { }
+  
   ngOnInit() {
+
   }
 
+  getCustomerOrders(){
+    //user email is nedeed...
+    let headers = new HttpHeaders().set('Authorization', 'Bearer ${this.auth.accessToken}')
+    
+    this.http.get<Order[]>(this.API_URL+'orders', {headers: headers})
+      .subscribe(data=>{
+          console.log('receiving the orders list!');
+          this.orders = data;
+        },
+        err=>{
+          console.log('error receiving the orders list!');
+          console.log(err);
+        }
+    ); 
+  }
+  
 }
--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Order } from '../list-pizzas/order';
 
 @Component({
   selector: 'app-my-orders',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-orders.component.css']
 })
 export class MyOrdersComponent implements OnInit {
-
-  constructor() { }
+  user_profile:any;
+  orders:Order[];
+  API_URL: string = "http://localhost:3001/";
+  constructor(public auth:AuthService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.getProfileInformation();
+    this.getMyOrders();
   }
 
+  getMyOrders(){
+    //user email is nedeed...
+    let headers = new HttpHeaders().set('Authorization', 'Bearer ${this.auth.accessToken}')
+    let params = new HttpParams().set("user_email",this.user_profile.email); //Create new HttpParams
+
+    this.http.get<Order[]>(this.API_URL+'myOrders', {headers: headers, params: params})
+      .subscribe(data=>{
+          console.log('receiving My Orders list!');
+          this.orders = data;
+        },
+        err=>{
+          console.log('error receiving My Orders list!');
+          console.log(err);
+        }
+    ); 
+    
+  }
+
+  getProfileInformation():void{
+    if (this.auth.userProfile) {
+      this.user_profile = this.auth.userProfile; 
+       console.log(this.auth.userProfile);
+     }
+  }
 }
