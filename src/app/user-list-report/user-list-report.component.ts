ce77d662bfe511a9ae09264008e600cb07dc3560
Antornio <gaanva@gmail.com>
2019-05-20 08:51:40 -0300

    creating and showing userList

--- src/app/user-list-report/user-list-report.component.ts
+++ src/app/user-list-report/user-list-report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersRegistered } from './users-registered';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-user-list-report',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list-report.component.css']
 })
 export class UserListReportComponent implements OnInit {
+  API_URL: string = "http://localhost:3001/";
+  userList: UsersRegistered[] = [];
 
-  constructor() { }
+  constructor(public auth:AuthService, private http: HttpClient) {
+    
+  }
 
   ngOnInit() {
+    this.getAllUsersList();
+  }
+
+  getAllUsersList(){
+    this.http.get<UsersRegistered[]>(`${this.API_URL}usersListInformation`, {
+        headers: new HttpHeaders()
+          .set('Authorization', `Bearer ${this.auth.accessToken}`)
+      })
+      .subscribe(
+        users => {
+          this.userList = users;
+          console.log(users);
+        },
+        error => {
+          alert('Error retrieving users list. Please chec the console error.');  
+          console.log(error);
+            
+        }
+      );
   }
 
 }
---
+++ src/app/user-list-report/users-registered.spec.ts
@@ -0,0 +1,7 @@
+import { UsersRegistered } from './users-registered';
+
+describe('UsersRegistered', () => {
+  it('should create an instance', () => {
+    expect(new UsersRegistered()).toBeTruthy();
+  });
+});
