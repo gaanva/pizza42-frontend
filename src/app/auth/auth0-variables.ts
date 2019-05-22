f6072e9704bd3bd2d6d2f9f475cb098126ac7c6f
Antornio <gaanva@gmail.com>
2019-05-22 17:27:34 -0300

    setting prod ip

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -8,6 +8,6 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'ZH0SW7Dbvj0DVKbaZ8viszeDRBAyO59Q',
   domain: 'gaanva.auth0.com',
-  callbackURL: 'http://localhost:3000/callback',
-  apiUrl: 'http://localhost:3001'
+  callbackURL: 'http://3.14.88.49:3000/callback',
+  apiUrl: 'http://3.14.88.49:3001'
 };
